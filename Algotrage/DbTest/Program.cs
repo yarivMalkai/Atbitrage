ed5e9eb334f67cd660b4eab4ffaac7840d4cd409
DESKTOP-HD86037\WSC User <yarivim@gmail.com>
2017-05-20 18:48:19 +0300

    Test project

---
+++ Algotrage/DbTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using AlgotrageDAL.Context;
+using AlgotrageDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AlgotrageContext())
+            {
+                //var team1 = new Team
+                //{
+                //    DisplayName = "First"
+                //};
+
+                //var team2 = new Team
+                //{
+                //    DisplayName = "Second"
+                //};
+
+                //db.Teams.Add(team1);
+                //db.Teams.Add(team2);
+
+                //var game = new Game
+                //{
+                //    AwayTeam = team2,
+                //    HomeTeam = team1,
+                //    Date = DateTime.Now
+                //};
+
+                //db.Games.Add(game);
+
+                //db.SaveChanges();
+
+                var games = db.Games.ToList();
+                var game = games.First();
+                Console.WriteLine($"{game.HomeTeam.DisplayName} - {game.AwayTeam.DisplayName}");
+            }
+        }
+    }
+}
