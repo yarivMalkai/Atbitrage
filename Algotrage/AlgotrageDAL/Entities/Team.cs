93a82531aff0b3b3b569d0bd17bd058ef31f7c02
DESKTOP-HD86037\WSC User <yarivim@gmail.com>
2017-05-20 16:06:41 +0300

    base entities

---
+++ Algotrage/AlgotrageDAL/Entities/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgotrageDAL.Entities
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public String DisplayName { get; set; }
+    }
+}
